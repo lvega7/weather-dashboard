ec1b69cbbcaab5da2b9442453be305fbd249f094
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-10-03 08:48:10 -0400

    add cards and dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,27 @@ var temp = document.querySelector(".temp")
 var wind = document.querySelector(".wind")
 var humid = document.querySelector(".humid")
 var uv = document.querySelector(".uv")
+var lat = document.querySelector(".lat")
+var lon = document.querySelector(".lon")
+var temp2 = document.querySelector(".temp2")
+var date = document.querySelector(".date")
+
+// loop to show 5-day dates
+var day = moment ().format('l');
+for (let i = 0; i < 5; i++) {
+    var multDays = moment().add(i, 'days').calendar('l');
+    console.log(multDays )
+    
+}
 
 
+// button to show the current weather
 button.addEventListener('click', function(event){
+    // fetch today's forecast
     fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&appid=732c0fa667469c7a58ce218262c96309'
+        'https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&units=imperial&appid=732c0fa667469c7a58ce218262c96309'
       )
-    //   fetch (
-        // 'https://api.openweathermap.org/data/2.5/onecall?lat=32.7153&lon=-117.1573&exclude={part}&appid=732c0fa667469c7a58ce218262c96309')
-
+      
         .then(response => response.json()) 
         .then(data => {
             var cityNameDateValue = data['name'];
@@ -25,27 +37,37 @@ button.addEventListener('click', function(event){
             temp.innerHTML = "Temp: "+ tempValue
             wind.innerHTML = "Wind: "+ windValue+ "mph"
             humid.innerHTML = "Humidity: "+humidValue + "%"
+            date.textContent= (day)
+            var latValue = data['coord']['lat']
+            var lonValue = data['coord']['lon']
             
             console.log(data)
-            fiveDayForecast();
+            // fetch the 5 day forecast
+            fetch (
+                'https://api.openweathermap.org/data/2.5/onecall?lat='+latValue+'&lon='+lonValue+'&units=imperial&exclude={part}&appid=732c0fa667469c7a58ce218262c96309')
+                .then(response => response.json()) 
+                .then(data =>{
+                    var temp2Value = data ['daily'][0]['temp']['day'] 
+                    // var time = data ['timezone_offset'+1000]
+                    
+
+                    temp2.innerHTML = temp2Value
+                    console.log(data)
+                    
+                    
+                })
+                
+                
+
+            // lat.innerHTML = latValue
+            // lon.innerHTML = lonValue
 
         })
    
 
-
+        // stops it from refreshing 
         event.preventDefault();
         
 })
 
-
-// var fiveDayForecast = function(){
-
-
-//     fetch (
-//         'https://api.openweathermap.org/data/2.5/onecall?lat=32.7153&lon=-117.1573&exclude={part}&appid=732c0fa667469c7a58ce218262c96309')
-
-//         .then(response => response.json()) 
-//         .then(data => console.log(data))
-
-//     }
 
