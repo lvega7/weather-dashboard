44545cfe438ff5c9aaba0f8f45bee650ed3d314e
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-10-03 17:00:43 -0400

    update 5 day forecast and uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,8 @@ var day = moment ().format('l');
 // loop to show 5-day dates
 // for (let i = 0; i < 5; i++) {
 //     var multDays = moment().add(i, 'days').calendar('l');
-//     console.log(JSON.stringify({multDays}))
+//     lon.innerHTML= multDays
+//     console.log(multDays)
     
 // }
 
@@ -60,26 +61,30 @@ button.addEventListener('click', function(event){
             var tempValue = data['main']['temp'];
             var windValue = data ['wind']['speed']
             var humidValue = data ['main']['humidity']
+            var iconValue = data ['weather'][0]['icon']
+            // var uvValue = data['daily']['uvi']
 
-            cityNameDate.innerHTML = cityNameDateValue + " " + day
+            cityNameDate.innerHTML = cityNameDateValue + " " + day + iconValue
             temp.innerHTML = "Temp: "+ tempValue + "°F"
             wind.innerHTML = "Wind: "+ windValue+ "mph"
             humid.innerHTML = "Humidity: "+humidValue + "%"
+            // uv.innerHTML = uvValue
             
             var latValue = data['coord']['lat']
             var lonValue = data['coord']['lon']
             
             console.log(data)
+            console.log(city.value)
             // fetch the 5 day forecast
             fetch (
                 'https://api.openweathermap.org/data/2.5/onecall?lat='+latValue+'&lon='+lonValue+'&units=imperial&exclude={part}&appid=732c0fa667469c7a58ce218262c96309')
                 .then(response => response.json()) 
                 .then(data =>{
-                    var temp1Value = data ['daily'][1]['temp']['day'] 
-                    var temp2Value = data ['daily'][2]['temp']['day'] 
-                    var temp3Value = data ['daily'][3]['temp']['day'] 
-                    var temp4Value = data ['daily'][4]['temp']['day'] 
-                    var temp5Value = data ['daily'][5]['temp']['day'] 
+                    var temp1Value ="Temp:"+data ['daily'][1]['temp']['day'] 
+                    var temp2Value ="Temp:"+data ['daily'][2]['temp']['day'] 
+                    var temp3Value ="Temp:"+data ['daily'][3]['temp']['day'] 
+                    var temp4Value ="Temp:"+data ['daily'][4]['temp']['day'] 
+                    var temp5Value ="Temp:"+data ['daily'][5]['temp']['day'] 
                     
                     
 
@@ -96,15 +101,80 @@ button.addEventListener('click', function(event){
                         
                     // }
 
-                    var wind1 = data ['daily'][1]['wind_speed']+ "mph"
-                    var wind2 = data ['daily'][2]['wind_speed']+ "mph"
-                    var wind3 = data ['daily'][3]['wind_speed']+ "mph"
-                    var wind4 = data ['daily'][4]['wind_speed']+ "mph"
-                    var wind5 = data ['daily'][5]['wind_speed']+ "mph"
+                    // wind forecast
+                    var wind1 = document.querySelector(".wind1")
+                    var wind2 = document.querySelector(".wind2")
+                    var wind3 = document.querySelector(".wind3")
+                    var wind4 = document.querySelector(".wind4")
+                    var wind5 = document.querySelector(".wind5")
 
+                    var wind1value ="Wind:"+data ['daily'][1]['wind_speed']+ "mph"
+                    var wind2value ="Wind:"+data ['daily'][2]['wind_speed']+ "mph"
+                    var wind3value ="Wind:"+data ['daily'][3]['wind_speed']+ "mph"
+                    var wind4value ="Wind:"+data ['daily'][4]['wind_speed']+ "mph"
+                    var wind5value ="Wind:"+data ['daily'][5]['wind_speed']+ "mph"
+
+                    wind1.innerHTML=wind1value
+                    wind2.innerHTML=wind2value
+                    wind3.innerHTML=wind3value
+                    wind4.innerHTML=wind4value
+                    wind5.innerHTML= wind5value  
+
+                    // humidity forecast
+                    var humid1 = document.querySelector(".humid1")
+                    var humid2 = document.querySelector(".humid2")
+                    var humid3 = document.querySelector(".humid3")
+                    var humid4 = document.querySelector(".humid4")
+                    var humid5 = document.querySelector(".humid5")
+
+                    var humid1value ="Humidity:"+data ['daily'][1]['humidity']+ "%"
+                    var humid2value ="Humidity:"+data ['daily'][2]['humidity']+ "%"
+                    var humid3value ="Humidity:"+data ['daily'][3]['humidity']+ "%"
+                    var humid4value ="Humidity:"+data ['daily'][4]['humidity']+ "%"
+                    var humid5value ="Humidity:"+data ['daily'][5]['humidity']+ "%"
+
+                    humid1.innerHTML=humid1value
+                    humid2.innerHTML=humid2value
+                    humid3.innerHTML=humid3value
+                    humid4.innerHTML=humid4value
+                    humid5.innerHTML=humid5value 
+
+                    // uv index
+                    var uvIndex = document.querySelector('.uvIndex')
+                    var uvIndexValue =data['daily'][0]['uvi']
+                    uvIndex.innerHTML = "UV: " + uvIndexValue
+                    
+                  
+                        if(uvIndexValue <= 2 ){
+                            uvIndex.style.backgroundColor=("green");
+                        }
+                        else if(uvIndexValue<= 5){
+                            uvIndex.style.backgroundColor=("yellow")
+                        }
+                        else if(uvIndexValue<= 7){
+                            uvIndex.style.backgroundColor=("orange")
+                        }
+                        else if(uvIndexValue<= 10){
+                            uvIndex.style.backgroundColor=("red")
+                        }
+                        else if(uvIndexValue>= 11 ){
+                            uvIndex.style.backgroundColor=("yellow")
+                        }
+                        
+                        
+                        
+                        
                     
 
 
+                    // saved items 
+                    document.querySelector(".list").value = localStorage.getItem('city');
+                    var list = document.querySelector (".list")
+                    var citySaved = localStorage.getItem('city')
+                    list.innerHTML = citySaved
+
+
+                    localStorage.setItem('city', city.value)
                     
                     
                 })
