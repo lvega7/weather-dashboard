76c01651945855014b365b6e3a869080a802313b
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-10-03 09:45:24 -0400

    update forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,44 @@ var humid = document.querySelector(".humid")
 var uv = document.querySelector(".uv")
 var lat = document.querySelector(".lat")
 var lon = document.querySelector(".lon")
+var temp1 = document.querySelector(".temp1")
 var temp2 = document.querySelector(".temp2")
+var temp3 = document.querySelector(".temp3")
+var temp4 = document.querySelector(".temp4")
+var temp5 = document.querySelector(".temp5")
 var date = document.querySelector(".date")
+var day1 = document.querySelector(".day1")
+var day2 = document.querySelector(".day2")
+var day3 = document.querySelector(".day3")
+var day4 = document.querySelector(".day4")
+var day5 = document.querySelector(".day5")
+
+// current date
+var day = moment ().format('l');
 
 // loop to show 5-day dates
-var day = moment ().format('l');
-for (let i = 0; i < 5; i++) {
-    var multDays = moment().add(i, 'days').calendar('l');
-    console.log(multDays )
+// for (let i = 0; i < 5; i++) {
+//     var multDays = moment().add(i, 'days').calendar('l');
+//     console.log(JSON.stringify({multDays}))
     
-}
+// }
+
+var day1date= moment().add(1, 'days').calendar('l');
+    day1.innerHTML = day1date
+
+var day2date= moment().add(2, 'days').calendar('l');
+    day2.innerHTML = day2date
+
+var day3date= moment().add(3, 'days').calendar('l');
+    day3.innerHTML = day3date
+
+var day4date= moment().add(4, 'days').calendar('l');
+    day4.innerHTML = day4date
+
+var day5date= moment().add(5, 'days').calendar('l');
+    day5.innerHTML = day5date
+
+
 
 
 // button to show the current weather
@@ -33,11 +61,11 @@ button.addEventListener('click', function(event){
             var windValue = data ['wind']['speed']
             var humidValue = data ['main']['humidity']
 
-            cityNameDate.innerHTML = cityNameDateValue
-            temp.innerHTML = "Temp: "+ tempValue
+            cityNameDate.innerHTML = cityNameDateValue + " " + day
+            temp.innerHTML = "Temp: "+ tempValue + "°F"
             wind.innerHTML = "Wind: "+ windValue+ "mph"
             humid.innerHTML = "Humidity: "+humidValue + "%"
-            date.textContent= (day)
+            
             var latValue = data['coord']['lat']
             var lonValue = data['coord']['lon']
             
@@ -47,12 +75,36 @@ button.addEventListener('click', function(event){
                 'https://api.openweathermap.org/data/2.5/onecall?lat='+latValue+'&lon='+lonValue+'&units=imperial&exclude={part}&appid=732c0fa667469c7a58ce218262c96309')
                 .then(response => response.json()) 
                 .then(data =>{
-                    var temp2Value = data ['daily'][0]['temp']['day'] 
-                    // var time = data ['timezone_offset'+1000]
+                    var temp1Value = data ['daily'][1]['temp']['day'] 
+                    var temp2Value = data ['daily'][2]['temp']['day'] 
+                    var temp3Value = data ['daily'][3]['temp']['day'] 
+                    var temp4Value = data ['daily'][4]['temp']['day'] 
+                    var temp5Value = data ['daily'][5]['temp']['day'] 
+                    
                     
 
+                    temp1.innerHTML = temp1Value
                     temp2.innerHTML = temp2Value
+                    temp3.innerHTML = temp3Value
+                    temp4.innerHTML = temp4Value
+                    temp5.innerHTML = temp5Value
                     console.log(data)
+
+                    // for (let i = 1; i < 6; i++) {
+                    //     var forecast = data ['daily'][i]['wind_speed']
+                    //     console.log(forecast)
+                        
+                    // }
+
+                    var wind1 = data ['daily'][1]['wind_speed']+ "mph"
+                    var wind2 = data ['daily'][2]['wind_speed']+ "mph"
+                    var wind3 = data ['daily'][3]['wind_speed']+ "mph"
+                    var wind4 = data ['daily'][4]['wind_speed']+ "mph"
+                    var wind5 = data ['daily'][5]['wind_speed']+ "mph"
+
+                    
+
+
                     
                     
                 })
